6e67527d4bed0e01bf96507e6be15f181a1f00eb
rodri nube <rodrigonuberuiz@gmail.com>
2023-04-05 13:18:36 -0600

    feat: added start message

--- src/main/java/me/rodri/proxy/VoltProxy.java
+++ src/main/java/me/rodri/proxy/VoltProxy.java
@@ -1,20 +1,26 @@
 package me.rodri.proxy;
 
+import me.rodri.proxy.commands.DiscordCommand;
 import me.rodri.proxy.commands.PingCommand;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.protocol.packet.Chat;
 
 public final class VoltProxy extends Plugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info(" successfully loaded!");
-
-
-
+        getLogger().info(ChatColor.GRAY +"------------------------------");
+        getLogger().info("     " + ChatColor.YELLOW + "Volt Proxy");
+        getLogger().info("");
+        getLogger().info("Join our discord " + ChatColor.BLUE + "dsc.gg/voltdev");
+        getLogger().info("Made by " + ChatColor.GOLD + "Volt Development");
+        getLogger().info(ChatColor.GRAY +  "------------------------------");
 
         //Register Commands
         getProxy().getPluginManager().registerCommand(this, new PingCommand());
+        getProxy().getPluginManager().registerCommand(this, new DiscordCommand());
 
     }
 
