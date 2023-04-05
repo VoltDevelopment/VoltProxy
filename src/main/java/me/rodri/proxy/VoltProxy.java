3bff89e3e20e4f1300914fefd8c97c64d96ec3f1
rodri nube <rodrigonuberuiz@gmail.com>
2023-04-05 11:43:20 -0600

    feat: register ping command

--- src/main/java/me/rodri/proxy/VoltProxy.java
+++ src/main/java/me/rodri/proxy/VoltProxy.java
@@ -1,5 +1,6 @@
 package me.rodri.proxy;
 
+import me.rodri.proxy.commands.PingCommand;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class VoltProxy extends Plugin {
@@ -9,11 +10,18 @@ public final class VoltProxy extends Plugin {
         // Plugin startup logic
         getLogger().info(" successfully loaded!");
 
+
+
+
+        //Register Commands
+        getProxy().getPluginManager().registerCommand(this, new PingCommand());
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info(" successfully disabled");
+
     }
 }
