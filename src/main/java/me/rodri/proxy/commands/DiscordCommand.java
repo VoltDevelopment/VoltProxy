c9b36625560ff49f18b0a43bbe5aaf14229ddcc6
rodri nube <rodrigonuberuiz@gmail.com>
2023-04-05 13:19:54 -0600

    feat: working on discord command

---
+++ src/main/java/me/rodri/proxy/commands/DiscordCommand.java
@@ -0,0 +1,16 @@
+package me.rodri.proxy.commands;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class DiscordCommand extends Command {
+
+    public DiscordCommand () {
+        super("discord");
+    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+
+    }
+}
