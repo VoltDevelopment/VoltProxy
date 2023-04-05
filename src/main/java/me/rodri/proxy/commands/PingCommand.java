c0a667e919d4cf153ddab5d76844418abd1769df
rodri nube <rodrigonuberuiz@gmail.com>
2023-04-05 11:44:03 -0600

    feat: added ping command

--- src/main/java/me/rodri/proxy/commands/PingCommand.java
+++ src/main/java/me/rodri/proxy/commands/PingCommand.java
@@ -1,4 +1,25 @@
 package me.rodri.proxy.commands;
 
-public class PingCommand {
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class PingCommand extends Command {
+
+    public PingCommand (){
+        super("ping");
+    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+        if (commandSender instanceof ProxiedPlayer ){
+            ProxiedPlayer player = (ProxiedPlayer) commandSender;
+
+            int ping = player.getPing();
+
+            player.sendMessage(ChatColor.YELLOW + "Your ping: " + ChatColor.GREEN + ping);
+
+        }
+    }
 }
