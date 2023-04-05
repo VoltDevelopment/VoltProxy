3700d2135cd92a63629ea80c4933c1545f45959a
rodri nube <rodrigonuberuiz@gmail.com>
2023-04-05 16:08:40 -0600

    feat: discord command finished

--- src/main/java/me/rodri/proxy/commands/DiscordCommand.java
+++ src/main/java/me/rodri/proxy/commands/DiscordCommand.java
@@ -1,6 +1,8 @@
 package me.rodri.proxy.commands;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class DiscordCommand extends Command {
@@ -11,6 +13,13 @@ public class DiscordCommand extends Command {
 
     @Override
     public void execute(CommandSender commandSender, String[] strings) {
+        if (commandSender instanceof ProxiedPlayer){
+            ProxiedPlayer player = (ProxiedPlayer) commandSender;
 
+            player.sendMessage("");
+            player.sendMessage(ChatColor.YELLOW + "Join our Discord: " + ChatColor.BLUE + "dsc.gg/voltdev");
+            player.sendMessage("");
+
+        }
     }
 }
