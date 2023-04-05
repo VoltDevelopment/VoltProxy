6dd78ee61d0e7e8d3b5ba905bfef7b06ace79d10
rodri nube <rodrigonuberuiz@gmail.com>
2023-04-05 16:09:16 -0600

    feat: working on twitter command

---
+++ src/main/java/me/rodri/proxy/commands/TwitterCommand.java
@@ -0,0 +1,15 @@
+package me.rodri.proxy.commands;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class TwitterCommand extends Command {
+
+    public TwitterCommand () {
+        super("twitter");
+    }
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+
+    }
+}
